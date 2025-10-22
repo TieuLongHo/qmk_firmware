30e6db897d798ec380524f1933c6644469f6400a
Tieu Long <tieu.ho@students.bfh.ch>
2025-10-22 23:37:12 +0200

    Fix keymap order in Symbol Layer for cygnus_4x6 keyboard

--- keyboards/handwired/cygnus_4x6/keymaps/default/keymap.c
+++ keyboards/handwired/cygnus_4x6/keymaps/default/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+-----+-----+-----+-----+-----|                |-----+------+-----+-----+-----+------|
  * |      |     |     |  [  |  ]  |     |                |  4  |  5   |  6  |  0  |  -  |      |
  * |------+-----+-----+-----+-----+-----|                |-----+------+-----+-----+-----+------|
- * |QK_BOOT|    |     |  {  |  }  |     |                |  3  |  2   |  1  |     |  /  |      |
+ * |QK_BOOT|    |     |  {  |  }  |     |                |  1  |  2   |  3  |     |  /  |      |
  * .------+-----+-----+-----+-----+-----|                |-----+------+-----+-----+-----+------.
  *                    |     |     |     |                |     |      |     |
  *                    '-----+-----+-----'                '-----+-----+------'
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
             KC_7,     KC_8,     KC_9,     KC_F12,   KC_PLUS,  KC_DEL,
             KC_4,     KC_5,     KC_6,     KC_0,     KC_MINS,  KC_TRNS,
-            KC_3,     KC_2,     KC_1,     KC_TRNS,  KC_SLSH,  KC_TRNS,
+            KC_1,     KC_2,     KC_3,     KC_TRNS,  KC_SLSH,  KC_TRNS,
             KC_TRNS,  KC_TRNS,  KC_TRNS
         ),
 
