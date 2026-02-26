1c13dfe9afe0b5304a21999dfd50c7e4b389d429
Tieu Long <tieu.ho@students.bfh.ch>
2026-02-26 01:07:53 +0100

    feat: implement OS detection and update keymap configurations for Cygnus 4x6 keyboard

--- keyboards/handwired/cygnus_4x6/via/default/keymap.c
+++ keyboards/handwired/cygnus_4x6/via/default/keymap.c
@@ -25,17 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                              '-----------------'        '-----------------'
      */
     [_WIN] = LAYOUT(
-        // Left hand
-            KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_6,       KC_7,       KC_8,       KC_9,    KC_0,    KC_BSPC,
-            KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_Y,       KC_U,       KC_I,       KC_O,    KC_P,    KC_ENT,
-            KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_H,       KC_J,       KC_K,       KC_L,    KC_SCLN, KC_QUOT,
-            KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_N,       KC_M,       KC_COMM,    KC_DOT,  KC_SLSH, KC_RSFT,
-                                          KC_LGUI, KC_LALT, KC_SPC, KC_SPC,   MO(_RAISE), MO(_LOWER)
-        // Right hand
-
-
-
-        ),
+        KC_ESC,  KC_1,    KC_2,       KC_3,       KC_4,       KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+        KC_LCTL, KC_A,    KC_S,       KC_D,       KC_F,       KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,       KC_C,       KC_V,       KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   KC_LGUI,    KC_LALT,    KC_SPC,    KC_SPC,  MO(_RAISE),   MO(_LOWER)),
     /* Keymap 1 : Mac Layer
      *                    .-----.                                                    .-----.
 d     *              .-----|     |-----------.                            .-----------|     |-----.
@@ -52,12 +46,11 @@ d     *              .-----|     |-----------.                            .-----
      *                              '-----------------'        '-----------------'
      */
     [_MAC] = LAYOUT(
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,
-                                           KC_LALT,  KC_LGUI,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS
-        ),
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                   KC_LALT,    KC_LGUI,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS),
     /* Keymap 2 : Symbol Layer
      *                    .-----.                                                    .-----.
      *              .-----| F3  |-----------.                            .-----------| F8  |-----.
@@ -74,13 +67,11 @@ d     *              .-----|     |-----------.                            .-----
      *                              '-----------------'        '-----------------'
      */
     [_RAISE] = LAYOUT(
-            KC_GRV,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,KC_F6,        KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_MINS,
-            KC_NUM_LOCK,    KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_TRNS,KC_7,       KC_8,     KC_9,     KC_F11,   KC_F12,   KC_PLUS,
-            KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_LBRC,  KC_RBRC,  KC_TRNS,KC_4,       KC_5,     KC_6,     KC_TRNS,  KC_TRNS,  KC_CAPS_LOCK,
-            QK_BOOT,        KC_TRNS,  KC_TRNS,  KC_LCBR,  KC_RCBR,  KC_TRNS,KC_1,       KC_2,     KC_3,     KC_0,     KC_TRNS,  KC_DEL,
-                                                KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,    KC_TRNS,  KC_TRNS
-        ),
-
+        KC_GRV,  KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS,
+        KC_NUM_LOCK, KC_TRNS, KC_TRNS,    S(KC_9),    S(KC_0),    KC_TRNS,    KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS, KC_EQL,
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_LBRC,    KC_RBRC,    KC_TRNS,    KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS, KC_CAPS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    S(KC_LBRC), S(KC_RBRC), KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_0,    KC_TRNS, KC_DEL,
+                                   KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS),
     /* Keymap 3 : OS Layer
      *                    .-----.                                                    .-----.
      *              .-----|     |-----------.                            .-----------|     |-----.
@@ -97,11 +88,19 @@ d     *              .-----|     |-----------.                            .-----
      *                              '-----------------'        '-----------------'
      */
     [_LOWER] = LAYOUT(
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-            KC_TRNS,   KC_TRNS,  TO(_WIN), KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,KC_LEFT,   KC_DOWN,  KC_UP,    KC_RIGHT,  KC_TRNS,  KC_TRNS,
-            KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,   TO(_MAC), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-                                           KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,   KC_TRNS,  KC_TRNS
-        )
-    };
+        KC_F1,   KC_F2,   KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_NUM_LOCK, KC_TRNS, TO(_WIN),   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_CAPS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, TO(_MAC), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+};
 
+bool process_detected_host_os_user(os_variant_t detected_os) {
+    if (detected_os == OS_MACOS || detected_os == OS_IOS) {
+        set_single_default_layer(_MAC);
+    } else if (detected_os == OS_WINDOWS || detected_os == OS_LINUX) {
+        set_single_default_layer(_WIN);
+    }
+
+    return true;
+}
